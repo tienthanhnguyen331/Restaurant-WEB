05f640118343c0e8c1b61095df6ea9888d2dc3e6
TienThanhNguyen <nguyentienthanhaeaefff@gmail.com>
2025-12-27 15:07:05 +0700

    fix: correct entity class names in app module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -11,13 +11,13 @@ import { MenuItemsModule } from './modules/menu-items/menu-items.module';
 import { MenuCategoriesModule } from './modules/menu-categories/menu-categories.module';
 import { MenuItemPhotosModule } from './modules/menu-item-photos/menu-item-photos.module';
 
-import { Table } from './tables/table.entity';
-import { MenuCategory } from './modules/menu-categories/entities/menu-category.entity';
-import { MenuItem } from './modules/menu-items/entities/menu-item.entity';
-import { MenuItemPhoto } from './modules/menu-item-photos/entities/menu-item-photo.entity';
-import { ModifierGroup } from './modules/modifiers/entities/modifier-group.entity';
-import { ModifierOption } from './modules/modifiers/entities/modifier-option.entity';
-import { MenuItemModifierGroup } from './modules/modifiers/entities/menu-item-modifier-group.entity';
+import { TableEntity } from './tables/table.entity';
+import { MenuCategoryEntity } from './modules/menu-categories/entities/menu-category.entity';
+import { MenuItemEntity } from './modules/menu-items/entities/menu-item.entity';
+import { MenuItemPhotoEntity } from './modules/menu-item-photos/entities/menu-item-photo.entity';
+import { ModifierGroupEntity } from './modules/modifiers/entities/modifier-group.entity';
+import { ModifierOptionEntity } from './modules/modifiers/entities/modifier-option.entity';
+import { MenuItemModifierGroupEntity } from './modules/modifiers/entities/menu-item-modifier-group.entity';
 
 @Module({
   imports: [
@@ -36,13 +36,13 @@ import { MenuItemModifierGroup } from './modules/modifiers/entities/menu-item-mo
         password: config.get<string>('DATABASE_PASSWORD'),
         database: config.get<string>('DATABASE_NAME'),
         entities: [
-          Table,
-          MenuCategory,
-          MenuItem,
-          MenuItemPhoto,
-          ModifierGroup,
-          ModifierOption,
-          MenuItemModifierGroup,
+          TableEntity,
+          MenuCategoryEntity,
+          MenuItemEntity,
+          MenuItemPhotoEntity,
+          ModifierGroupEntity,
+          ModifierOptionEntity,
+          MenuItemModifierGroupEntity,
         ],
         synchronize: false, // Disable synchronize in production/serverless
         ssl: true, // Force SSL for Neon/Cloud DB
